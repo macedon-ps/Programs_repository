73682869f687500f43308ab5590e08d71e598490
AndreyKazanchan <macedon1971@gmail.com>
2021-08-03 16:00:35 +0300

    create index.php, addUser.php, showUsers.php

---
+++ PHP/Homework/HW_1/addUser.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Регистрация пользователей</title>
+</head>
+<body>
+    <h1>Страница регистрации пользователей</h1>
+</body>
+</html>
---
+++ PHP/Homework/HW_1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Пользователи</title>
+</head>
+<body>
+    <nav>
+        <div class="addUser">
+            <button>
+                <a href="addUser.php">Add new user</a>
+            </button>
+        </div>
+        <div class="showUsers">
+        <button>
+                <a href="showUsers.php">Show all users</a>
+            </button>
+        </div>
+    </nav>
+</body>
+</html>
