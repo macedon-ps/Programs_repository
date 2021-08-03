1f6433fb30f3817ee2b4a9b727a8edef1ad7c770
AndreyKazanchan <macedon1971@gmail.com>
2021-08-03 16:09:58 +0300

    add forms for addUser.php

--- PHP/Homework/HW_1/showUsers.php
+++ PHP/Homework/HW_1/showUsers.php
@@ -2,11 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Все пользователи сервиса</title>
+    <title>Регистрация пользователей</title>
 </head>
 <body>
-    <h1>Страница всех пользователей</h1>
+    <h1>Страница регистрации пользователей</h1>
+    <form action="/">
+        <label for="login">Введите свой логин: </label><br/>
+        <input type="text" name="login"><br/>
+        <label for="password">Введите свой пароль: </label><br/>
+        <input type="password" name="password"><br/>
+        <label for="email">Введите свой электронный адрес: </label><br/>
+        <input type="email" name="email"><br/>
+    </form>
 </body>
 </html>
