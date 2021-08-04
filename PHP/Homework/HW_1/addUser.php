52e6431e6ccb938b4a74e4278bc1b9859d32e483
AndreyKazanchan <macedon1971@gmail.com>
2021-08-04 23:28:55 +0300

    add verification, write to users.txt

--- PHP/Homework/HW_1/addUser.php
+++ PHP/Homework/HW_1/addUser.php
@@ -5,7 +5,7 @@
     <title>Регистрация пользователей</title>
 </head>
 <body>
-<h1>Страница регистрации пользователей</h1>
+    <h1>Страница регистрации пользователей</h1>
     
     <?php 
         if(!isset($_POST["submit"])) {
@@ -24,15 +24,60 @@
     </form>
     <?php
         }
-        elseif(isset($_POST["submit"]) and $_POST["login"] != "" and $_POST["password"] != "" and $_POST["email"] != ""){
-            echo("<br/>");
-            echo("Вы  " . $_POST["login"] . "  успешно зарегистрированы!");
+        elseif(isset($_POST["submit"])) 
+        {
+        
+            // проверка (верификация) данных
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            
+            // убираем пробелы, защищаем от SQL уязвимостей
+            
+            $login = trim(htmlspecialchars($_POST["login"]));
+            $password = trim(htmlspecialchars($_POST["password"]));
+            $email = trim(htmlspecialchars($_POST["email"]));
+
+            // проверям, что формы не пустые
+            if($_POST["login"] == "" || $_POST["password"] == "" || $_POST["email"] == ""){
+                echo "Вы ввели не все необходимые данные";
+                return false;
+            }
+                
+            // проверяем, что формы длиной больше 3 символов, меньше 30 символов
+            if(strlen($_POST["login"]) < 3 || strlen($_POST["login"]) > 30 || strlen($_POST["password"]) < 3 || strlen($_POST["login"]) > 30){
+                echo "Вы ввели меньше 8 или больше 30 знаков в логине / или пароле";
+                return false;
+            }
+
+            // запись в файл
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            $file = "D:\OpenServer\domains\menuUsers\users.txt";
+            // global $file;
+            
+            if(!file_exists($file)){
+                echo "Такого файла не существует\n";
+                return false;
+            }
+
+            $f = fopen($file, "a+");
+            $str = $_POST["login"] . ": " . md5($_POST["passwors"]) . ": " . $_POST["email"];
+            echo $str;
+            
+            fputs($f, $str . "\n");
+            fclose($f);
+            
+            // отрисовка сообщения об успешной регистрации
+            ///////////////////////////////////////////////////////////////////////////////////////////// 
+            // echo("<br/>");
+            // echo("Вы  " . $_POST["login"] . "  успешно зарегистрированы!");
     ?>
-        <br/>
-        <br/>
-        <a href="index.php">На главную страницу</a>    
-    <?php    
-        $_POST[] = NULL;
+            <br/>
+            <br/>
+            <a href="index.php">На главную страницу</a>    
+    <?php   
+            $_POST[] = NULL;
+            return true;
+            
+            ////////////////////////////////////////////////////////////////////////////////////////////
         }
         else
         {
