227f2c6622e6edfbde950b8074f9f92c928ea87a
AndreyKazanchan <macedon1971@gmail.com>
2021-08-05 19:49:25 +0300

    put some code in functions

--- PHP/Homework/HW_1/addUser.php
+++ PHP/Homework/HW_1/addUser.php
@@ -24,69 +24,19 @@
     </form>
     <?php
         }
+        
         elseif(isset($_POST["submit"])) 
         {
-        
-            // проверка (верификация) данных
-            /////////////////////////////////////////////////////////////////////////////////////////////
-            // убираем пробелы, защищаем от SQL уязвимостей
-            
-            $login = trim(htmlspecialchars($_POST["login"]));
-            $password = trim(htmlspecialchars($_POST["password"]));
-            $email = trim(htmlspecialchars($_POST["email"]));
-
-            // проверям, что формы не пустые
-            if($_POST["login"] == "" || $_POST["password"] == "" || $_POST["email"] == ""){
-                echo "Вы ввели не все необходимые данные";
-                return false;
+            include_once("functions.php");
+            if(verification(($_POST["login"]), ($_POST["password"]), ($_POST["email"]))){
+                register(($login), ($password), ($email));
             }
-                
-            // проверяем, что формы длиной больше 3 символов, меньше 30 символов
-            if(strlen($_POST["login"]) < 3 || strlen($_POST["login"]) > 30 || strlen($_POST["password"]) < 3 || strlen($_POST["login"]) > 30){
-                echo "Вы ввели меньше 8 или больше 30 знаков в логине / или пароле";
-                return false;
-            }
-
-            // запись в файл
-            /////////////////////////////////////////////////////////////////////////////////////////////
-            $file = "D:\OpenServer\domains\menuUsers\users.txt";
-            // global $file;
             
-            if(!file_exists($file)){
-                echo "Такого файла не существует\n";
-                return false;
-            }
-
-            $f = fopen($file, "a+");
-            $str = $_POST["login"] . ": " . md5($_POST["passwors"]) . ": " . $_POST["email"];
-            echo $str;
-            
-            fputs($f, $str . "\n");
-            fclose($f);
-            
-            // отрисовка сообщения об успешной регистрации
-            ///////////////////////////////////////////////////////////////////////////////////////////// 
-            echo("<br/>");
-            echo("Вы  " . $_POST["login"] . "  успешно зарегистрированы!");
-    ?>
-            <br/>
-            <br/>
-            <a href="index.php">На главную страницу</a>    
-    <?php   
-            $_POST[] = NULL;
-            return true;
-            
-            ////////////////////////////////////////////////////////////////////////////////////////////
         }
         else
         {
-    ?>        
-        <br/>
-        <p>Вы не зарегистрированы!</p>
-        <br/>
-        <br/>
-        <a href="index.php">На главную страницу</a>
-    <?php
+            echo("<br/>" . "<p>Вы не зарегистрированы!</p>");
+            echo("<br/>" . "<br/>" . '<a href="index.php">На главную страницу</a>');
         }
     ?>
 </body>
