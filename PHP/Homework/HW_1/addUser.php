2b4eb486f3b22d5f2384dbbd806f222349ce853d
AndreyKazanchan <macedon1971@gmail.com>
2021-08-06 16:52:00 +0300

    add link for css, add class for inputs

--- PHP/Homework/HW_1/addUser.php
+++ PHP/Homework/HW_1/addUser.php
@@ -3,44 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <title>Регистрация пользователей</title>
+    <?php 
+        echo "<link rel='stylesheet' href='style.css'>";   
+    ?>
 </head>
 <body>
-    <h1>Страница регистрации пользователей</h1>
-    
-    <?php 
-        if(!isset($_POST["submit"])) {
-    ?>
-    <form action="addUser.php" method="POST">
-        <label for="login">Введите свой логин: </label><br/>
-        <input type="text" name="login"><br/>
-        <label for="password">Введите свой пароль: </label><br/>
-        <input type="password" name="password"><br/>
-        <label for="email">Введите свой электронный адрес: </label><br/>
-        <input type="email" name="email"><br/>
-        <label for="submit"></label>
-        <input type="submit" name="submit">
-        <label for="reset"></label>
-        <input type="reset" name="reset">
-    </form>
-    <br/>
-    <br/>
-    <a href="index.php">На главную страницу</a>
-    <?php
-        }
+    <div class="container-page">
+        <h1>Страница регистрации пользователей</h1>
         
-        elseif(isset($_POST["submit"])) 
-        {
-            include_once("functions.php");
-            if(verification(($_POST["login"]), ($_POST["password"]), ($_POST["email"]))){
-                register(($login), ($password), ($email));
+        <?php 
+            if(!isset($_POST["submit"])) {
+        ?>
+        <form action="addUser.php" method="POST">
+            <label for="login" class="input-item">Введите свой логин: </label><br/>
+            <input type="text" name="login" class="input-item"><br/>
+            <label for="password" class="input-item">Введите свой пароль: </label><br/>
+            <input type="password" name="password" class="input-item"><br/>
+            <label for="email" class="input-item">Введите свой электронный адрес: </label><br/>
+            <input type="email" name="email" class="input-item"><br/>
+            <label for="submit"></label>
+            <input type="submit" name="submit"">
+            <label for="reset" class="input-item"></label>
+            <input type="reset" name="reset">
+        </form>
+        <br/>
+        <br/>
+        <a href="index.php">На главную страницу</a>
+        <?php
             }
             
-        }
-        else
-        {
-            echo("<br/>" . "<p>Вы не зарегистрированы!</p>");
-            echo("<br/>" . "<br/>" . '<a href="index.php">На главную страницу</a>');
-        }
-    ?>
+            elseif(isset($_POST["submit"])) 
+            {
+                include_once("functions.php");
+                if(verification(($_POST["login"]), ($_POST["password"]), ($_POST["email"]))){
+                    register(($login), ($password), ($email));
+                }
+                
+            }
+            else
+            {
+                echo("<br/>" . "<p>Вы не зарегистрированы!</p>");
+                echo("<br/>" . "<br/>" . '<a href="index.php">На главную страницу</a>');
+            }
+        ?>
+    </div>
 </body>
 </html>
