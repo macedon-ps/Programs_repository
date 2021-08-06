d017b4e524565538b38a2247a3da2ce3c6a143a5
AndreyKazanchan <macedon1971@gmail.com>
2021-08-06 16:50:24 +0300

    add link for css, add some classes

--- PHP/Homework/HW_1/index.php
+++ PHP/Homework/HW_1/index.php
@@ -3,33 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <title>Пользователи</title>
+    <?php 
+        echo "<link rel='stylesheet' href='style.css'>";   
+    ?>
 </head>
 <body>
-    <nav>
-        <div class="info-users">
-            <p>
-               На сайте зарегистрировано: 
-               <span>
-                   <?php include_once("functions.php");
-                         $count = countOfUsers();
-                         echo($count);?>
-                </span> пользователей
-            </p>
-        </div>
-        <div class="addUser">
-            <button>
-                <?php 
-                    echo'<a href="addUser.php">Add new user</a>';
-                ?>
-            </button>
-        </div>
-        <div class="showUsers">
-        <button>
-                <?php
-                    echo'<a href="showUsers.php">Show all users</a>';
-                ?>
-            </button>
-        </div>
-    </nav>
+    <div class="container-page">
+        
+        <h1>Выберите действие, нажав на кнопку меню</h1>    
+            <div class="info-users">
+                <p>
+                На сайте зарегистрировано: 
+                <span>
+                    <?php include_once("functions.php");
+                            $count = countOfUsers();
+                            echo($count);?>
+                    </span> пользователей
+                </p>
+            </div>
+            <div class="addUser">
+                <button class="btn">
+                    <?php 
+                        echo'<a href="addUser.php">Add new user</a>';
+                    ?>
+                </button>
+            </div>
+            <div class="showUsers">
+                <button class="btn">
+                    <?php
+                        echo'<a href="showUsers.php">Show all users</a>';
+                    ?>
+                </button>
+            </div>
+            
+    </div>
 </body>
 </html>
