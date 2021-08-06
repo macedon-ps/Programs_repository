804e8e94c09028a6fdd3f5f17b67363e13780bdd
AndreyKazanchan <macedon1971@gmail.com>
2021-08-06 16:52:51 +0300

    add link for css, add some classes

--- PHP/Homework/HW_1/showUsers.php
+++ PHP/Homework/HW_1/showUsers.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php 
+    echo "<link rel='stylesheet' href='style.css'>";   
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,20 +10,21 @@
     <title>Все пользователи сервиса</title>
 </head>
 <body>
-    <h1>Страница всех пользователей</h1>
-    <table border="1">
-        <caption>Зарегистрированные пользователи: </caption>
-        <tr><td>Логины пользователей</td><td>Хешированные пароли пользователей</td><td>Электронные адреса пользователей</td></tr>
+    <div class="container-page">
+        <h1>Страница всех пользователей</h1>
+        <table border="1">
+            <caption class="titleTable">Зарегистрированные пользователи: </caption>
+            <tr><td>Логины пользователей</td><td>Хешированные пароли пользователей</td><td>Электронные адреса пользователей</td></tr>
 
-    <?php
-        include_once("functions.php");
-        showUsers();
-    ?>
+        <?php
+            include_once("functions.php");
+            showUsers();
+        ?>
 
-    </table>
-    <br/>
-    <br/>
-    <a href="index.php">На главную страницу</a>
-
+        </table>
+        <br/>
+        <br/>
+        <a href="index.php">На главную страницу</a>
+    </div>
 </body>
 </html>
