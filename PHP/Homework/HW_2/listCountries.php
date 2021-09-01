3f8f8a39c971680e1ecfd7243e85ac8044f33741
AndreyKazanchan <macedon1971@gmail.com>
2021-09-01 23:24:14 +0300

    change file name

---
+++ PHP/Homework/HW_2/listCountries.php
@@ -0,0 +1,39 @@
+<?php
+
+    include_once("function.php");
+    connect();
+
+    $err = mysqli_connect_error($result);
+    if ($err) {
+        echo 'Ошибка подключения к базе данных. Код ошибки:' . $err . '<br>';
+        exit();
+    }
+
+    $selectAllCountries = 'select * from countries';
+    $result = mysqli_query($link, $selectAllCountries);
+
+    $err = mysqli_connect_error($result);
+    if ($err) {
+        echo 'Ошибка SQL запроса. Код ошибки:' . $err . '<br>';
+        exit();
+    }
+
+    echo '<form action="searchCountries.php" method="post" class="col-12 col-sm-12 col-md-6 col-lg-6">';
+    echo '<table class="table table-striped">';
+    echo '<tr>';
+    echo '<td class="col-2 col-sm-2 col-md-2 col-lg-2">№ п/п</td>';
+    echo '<td class="col-10 col-sm-10 col-md-10 col-lg-10">Страны:</td>';
+    echo '</tr>';
+
+    $number = 1;
+    while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+        echo '<tr>';
+        echo '<td class="col-2 col-sm-2 col-md-2 col-lg-2">' . $number++ . '</td>' . '<td class="col-10 col-sm-10 col-md-10 col-lg-10">' . $row[1] . '</td>';
+        echo '</tr>';
+    }
+
+    echo '</table>';
+    echo '</form>';
+
+?>
+    
