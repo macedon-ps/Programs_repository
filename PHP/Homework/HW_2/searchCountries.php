801547963e983fb4086ff35a5ce6bcddb9d75eed
AndreyKazanchan <macedon1971@gmail.com>
2021-09-01 23:26:32 +0300

    change searchCountries.php

--- PHP/Homework/HW_2/searchCountries.php
+++ PHP/Homework/HW_2/searchCountries.php
@@ -1,43 +1,8 @@
-<?php
-
-    include_once("function.php");
-    connect();
-
-    $err = mysqli_connect_error($result);
-    if ($err) {
-        echo 'Ошибка подключения к базе данных. Код ошибки:' . $err . '<br>';
-        exit();
-    }
-
-    $selectAllCountries = 'select * from countries';
-    $result = mysqli_query($link, $selectAllCountries);
-
-    $err = mysqli_connect_error($result);
-    if ($err) {
-        echo 'Ошибка SQL запроса. Код ошибки:' . $err . '<br>';
-        exit();
-    }
-
-    echo '<form action="searchCountries.php" method="post">';
-    echo '<table class="table table-striped">';
-    echo '<tr>';
-    echo '<td>№ п/п</td>';
-    echo '<td>Страны:</td>';
-    echo '</tr>';
-
-    while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
-        echo '<tr>';
-        echo '<td>' . $row[0] . '</td>' . '<td>' . $row[1] . '</td>';
-        echo '</tr>';
-    }
-
-    echo '</table>';
-    echo '</form>';
-
+<?php 
+    echo '<div class="search-countries bg-light">';
     echo '<form class="d-flex">';
-    // echo '<input type="submit" name="search" value="Поиск">';
-    echo '<input class="form-control me-2" type="search" name="search-country" placeholder="Найти страну" aria-label="Search">';
-    echo '<button class="btn btn-success" type="submit" name="search-button">Поиск</button>';
+    echo '<input class="form-control me-2" type="search" placeholder="Найти страну" aria-label="Search">'; 
+    echo '<button class="btn btn-success" type="submit">Найти</button>';
     echo '</form>';
+    echo '</div>';
 ?>
-    
