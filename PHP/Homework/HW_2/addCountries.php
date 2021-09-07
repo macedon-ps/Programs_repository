298496e8fd2dfcbbd687d164adc057c660d1ce73
AndreyKazanchan <macedon1971@gmail.com>
2021-09-07 13:45:12 +0300

    add some changes for press on add-country buttun

--- PHP/Homework/HW_2/addCountries.php
+++ PHP/Homework/HW_2/addCountries.php
@@ -1,8 +1,19 @@
 <?php
-    echo '<div class="add-countries bg-light">';
-    echo '<form class="d-flex">';
-    echo '<input class="form-control me-2" type="search" placeholder="Добавить страну" aria-label="Search">'; 
-    echo '<button class="btn btn-success" type="submit">Добавить</button>';
+    include_once("function.php");
+    connect();
+    // проверка на ошибки подключения к БД
+        
+    echo '<div class="add-countries bg-light col-10 col-sm-10 col-md-8 col-lg-6">';
+    echo '<form action="addCountries.php" method="post" class="d-flex">';
+    echo '<input class="form-control me-2" name="add-country" type="search" required placeholder="Добавить страну" aria-label="Search">'; 
+    echo '<input type="submit" class="btn btn-success" name="add-button">Добавить</input>';
     echo '</form>';
     echo '</div>';
+    
+    if(isset($_POST['add-button'])){
+        
+        writeNewCountry($_POST["add-country"]);
+        // echo'Вы нажали на $_POST["add-button"] = ' . $_POST['add-button'];
+        // echo'Название страны - ' . $_POST["add-country"];
+    }
 ?>   
