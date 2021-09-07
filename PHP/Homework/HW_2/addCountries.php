622866b439ce9197f028ce9d28f416f3657968e7
AndreyKazanchan <macedon1971@gmail.com>
2021-09-07 20:52:11 +0300

    add session_start() for saving value input

--- PHP/Homework/HW_2/addCountries.php
+++ PHP/Homework/HW_2/addCountries.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     include_once("function.php");
     connect();
     
@@ -7,9 +8,16 @@
         echo 'Ошибка подключения к базе данных. Код ошибки:' . $err . '<br>';
         exit();
     }
+
+    if(isset($_POST["add-button"])){
+        $_SESSION["nameCountry"] = $_POST["add-country"];
+    }
+
     echo '<div class="add-countries bg-light col-10 col-sm-10 col-md-8 col-lg-6">';
     echo '<form action="index.php?page=3" method="post" class="d-flex">';
-    echo '<input class="form-control me-2" name="add-country" type="search" required placeholder="Добавить страну" aria-label="Search">'; 
+    
+    echo '<input class="form-control me-2" name="add-country" value="' . $_SESSION["nameCountry"] . '" type="search" required placeholder="Добавить страну" aria-label="Search">'; 
+            
     echo '<input type="submit" class="btn btn-success" name="add-button"></input>';
     echo '</form>';
     echo '</div>';
