a05de59ac21f075205a724e67929f45d093e07c8
AndreyKazanchan <macedon1971@gmail.com>
2021-09-07 13:48:28 +0300

    add required for input search, add adaptive props

--- PHP/Homework/HW_2/searchCountries.php
+++ PHP/Homework/HW_2/searchCountries.php
@@ -1,7 +1,7 @@
 <?php 
-    echo '<div class="search-countries bg-light">';
+    echo '<div class="search-countries bg-light col-10 col-sm-10 col-md-8 col-lg-6">';
     echo '<form class="d-flex">';
-    echo '<input class="form-control me-2" type="search" placeholder="Найти страну" aria-label="Search">'; 
+    echo '<input class="form-control me-2" type="search" required placeholder="Найти страну" aria-label="Search">'; 
     echo '<button class="btn btn-success" type="submit">Найти</button>';
     echo '</form>';
     echo '</div>';
