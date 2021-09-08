e229ab563a03ec42dcc2a3310e57d087b343dea4
AndreyKazanchan <macedon1971@gmail.com>
2021-09-08 13:05:19 +0300

    removed unused code, change session var

--- PHP/Homework/HW_2/addCountries.php
+++ PHP/Homework/HW_2/addCountries.php
@@ -1,22 +1,13 @@
 <?php
-    session_start();
-    include_once("function.php");
-    connect();
     
-    $err = mysqli_connect_error($result);
-    if ($err) {
-        echo 'Ошибка подключения к базе данных. Код ошибки:' . $err . '<br>';
-        exit();
-    }
-
     if(isset($_POST["add-button"])){
-        $_SESSION["nameCountry"] = $_POST["add-country"];
+        $_SESSION["addNameCountry"] = $_POST["add-country"];
     }
 
     echo '<div class="add-countries bg-light col-10 col-sm-10 col-md-8 col-lg-6">';
     echo '<form action="index.php?page=3" method="post" class="d-flex">';
     
-    echo '<input class="form-control me-2" name="add-country" value="' . $_SESSION["nameCountry"] . '" type="search" required placeholder="Добавить страну" aria-label="Search">'; 
+    echo '<input class="form-control me-2" name="add-country" value="' . $_SESSION["addNameCountry"] . '" type="search" required placeholder="Добавить страну" aria-label="Search">'; 
             
     echo '<input type="submit" class="btn btn-success" name="add-button"></input>';
     echo '</form>';
