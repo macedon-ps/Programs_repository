309ad73e91ab066372f1cccebd924565c795a406
AndreyKazanchan <macedon1971@gmail.com>
2021-09-08 13:08:02 +0300

    add session var

--- PHP/Homework/HW_2/searchCountries.php
+++ PHP/Homework/HW_2/searchCountries.php
@@ -1,8 +1,14 @@
 <?php 
+    
+    if(isset($_POST["search-button"])){
+        $_SESSION["searchNameCountry"] = $_POST["search-country"];
+    }
+    
     echo '<div class="search-countries bg-light col-10 col-sm-10 col-md-8 col-lg-6">';
-    echo '<form class="d-flex">';
-    echo '<input class="form-control me-2" type="search" required placeholder="Найти страну" aria-label="Search">'; 
-    echo '<button class="btn btn-success" type="submit">Найти</button>';
+    echo '<form action="index.php?page=2" method="post" class="d-flex">';
+    echo '<input class="form-control me-2" type="search" name="search-country" value="' . $_SESSION["searchNameCountry"] . '" required placeholder="Найти страну" aria-label="Search">'; 
+    echo '<button class="btn btn-success" type="submit" name="search-button">Найти</button>';
     echo '</form>';
     echo '</div>';
+
 ?>
